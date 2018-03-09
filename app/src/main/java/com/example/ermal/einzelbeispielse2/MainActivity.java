fe61944aeeb5155cb9739306492ce1f6e3e63310
ermal <ermal.bajraktari06@gmail.com>
2018-03-09 15:40:03 +0100

    Commit (1):Bei Eingabe eines Leerstring oder keines Strings,wird der User durch ein pop up informiert.

--- app/src/main/java/com/example/ermal/einzelbeispielse2/MainActivity.java
+++ app/src/main/java/com/example/ermal/einzelbeispielse2/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.ermal.einzelbeispielse2;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button checkP;
+    EditText insertWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkP =(Button) findViewById(R.id.button);
+        insertWord= (EditText) findViewById(R.id.editText2);
+
+
+
+        checkP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = insertWord.getText().toString();
+
+                if (isEmpty(str)) {
+                    Toast.makeText(MainActivity.this, "Write Something first", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    if (!isPalindrome(str.trim())) {
+                        Toast.makeText(MainActivity.this, "Is Not palindrome", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Is Palindrome", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
     }
+
+    private boolean isPalindrome(String str) {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+    private boolean isEmpty(String etText) {
+        if (etText.trim().length() > 0)
+            return false;
+
+        return true;
+    }
+
 }
